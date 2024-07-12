- [FIX] actual resolution calculation - [ADD] sample code - [ADD] Iterable protocol

StateSpace.py
@@ -12,6 +12,18 @@ class StateSpace:
         self.bounds = bounds
         self.buildStateSpace(max_deltas)
 
+    def __iter__(self):
+        self._iter_state = 0
+        return self
+
+    def __next__(self):
+        max_index = np.prod(self.dim) - 1
+        if self._iter_state > max_index:
+            raise StopIteration
+        index = np.asarray(np.unravel_index(self._iter_state, self.dim))
+        self._iter_state += 1
+        return (index, self.toState(index))
+
     @property
     def ranges(self):
         return self.bounds[:,1] - self.bounds[:,0]
@@ -19,7 +31,7 @@ class StateSpace:
     def buildStateSpace(self, max_deltas):
         self.dim = self.ranges / max_deltas # Element-wise division
         self.dim = np.ceil(self.dim).astype(int) + 1 # If the division is not an integer, we increase the resolution to make the space uniform
-        self.deltas = self.ranges / self.dim # The actual sampling resolution
+        self.deltas = self.ranges / (self.dim-1) # The actual sampling resolution
         # linspaces = []
         # for i in range(self.n_states):
         #     linspaces.append(np.linspace(self.bounds[i,0],self.bounds[i,1],num=self.dim[i], endpoint=True))
@@ -40,3 +52,10 @@ class StateSpace:
         if np.any(index < 0) or np.any(index >= self.dim):
             raise ValueError("Index out of bounds.")
         return self.bounds[:,0] + index * self.deltas
+    
+if __name__ == "__main__":
+    bounds = np.array([[-1.0,1.0],[0.0,1.0]])
+    deltas = [0.1,0.5]
+    ss = StateSpace(2,bounds,deltas)
+    for i, state in ss:
+        print(i, state)