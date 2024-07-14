cdfb4c58393afb9bd1ac97b8ceea9801f5f1a9af
Antonio Langella <a.langella31@studenti.unisa.it>
2024-07-15 00:12:05 +0200

    [REF] renamed 'dim' to 'dims'

--- StateSpace.py
+++ StateSpace.py
@@ -17,10 +17,10 @@ class StateSpace:
         return self
 
     def __next__(self):
-        max_index = np.prod(self.dim) - 1
+        max_index = np.prod(self.dims) - 1
         if self._iter_state > max_index:
             raise StopIteration
-        index = np.asarray(np.unravel_index(self._iter_state, self.dim))
+        index = np.asarray(np.unravel_index(self._iter_state, self.dims))
         self._iter_state += 1
         return (index, self.toState(index))
 
@@ -29,12 +29,12 @@ class StateSpace:
         return self.bounds[:,1] - self.bounds[:,0]
 
     def buildStateSpace(self, max_deltas):
-        self.dim = self.ranges / max_deltas # Element-wise division
-        self.dim = np.ceil(self.dim).astype(int) + 1 # If the division is not an integer, we increase the resolution to make the space uniform
-        self.deltas = self.ranges / (self.dim-1) # The actual sampling resolution
+        self.dims = self.ranges / max_deltas # Element-wise division
+        self.dims = np.ceil(self.dims).astype(int) + 1 # If the division is not an integer, we increase the resolution to make the space uniform
+        self.deltas = self.ranges / (self.dims-1) # The actual sampling resolution
         # linspaces = []
         # for i in range(self.n_states):
-        #     linspaces.append(np.linspace(self.bounds[i,0],self.bounds[i,1],num=self.dim[i], endpoint=True))
+        #     linspaces.append(np.linspace(self.bounds[i,0],self.bounds[i,1],num=self.dims[i], endpoint=True))
         # self.X = np.stack(np.meshgrid(*linspaces,indexing='xy'))
 
     def toIndex(self, state: np.array):
@@ -49,7 +49,7 @@ class StateSpace:
         """Converts an index to a state in the state space."""
         if len(index) != self.n_states:
             raise ValueError("Index has the wrong dimension.")
-        if np.any(index < 0) or np.any(index >= self.dim):
+        if np.any(index < 0) or np.any(index >= self.dims):
             raise ValueError("Index out of bounds.")
         return self.bounds[:,0] + index * self.deltas
     
