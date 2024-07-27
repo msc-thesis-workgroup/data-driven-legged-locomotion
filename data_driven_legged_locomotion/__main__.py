bb53be01da4b6d1ae4eea27f07d614c839514714
Antonio Langella <a.langella31@studenti.unisa.it>
2024-07-27 14:31:50 +0200

    [ADD] Video rendering

--- data_driven_legged_locomotion/__main__.py
+++ data_driven_legged_locomotion/__main__.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+import mediapy as media
 import numpy as np
 from pathlib import Path
+import mujoco
 import mujoco.viewer as viewer
 import time
 
@@ -7,12 +10,24 @@ from data_driven_legged_locomotion.agents.h1_walk import MujocoMPCService
 from data_driven_legged_locomotion.common import MujocoEnvironment, ServiceSet, GreedyMaxEntropyCrowdsouring
 from data_driven_legged_locomotion.tasks.h1_walk import H1WalkEnvironment, h1_walk_cost
 
+# Environment
 #env = PendulumEnvironment()
 env = H1WalkEnvironment()
 ss = env.ss
 model = env.model
 cost = h1_walk_cost
 
+# Video
+video_fps = 60
+video_resolution = (720, 1280)
+frame_count = 0
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+video_path = Path(__file__).parent.parent / "videos" / f"h1_walk_{current_datetime}.mp4"
+if not video_path.parent.exists():
+    video_path.parent.mkdir()
+renderer = mujoco.Renderer(model, height=video_resolution[0], width=video_resolution[1])
+
+# Crowdsourcing
 services = ServiceSet(ss)
 # swingUpService = SwingUpService(ss, model)
 # lqrService = LQRService(ss, model)
@@ -41,26 +56,34 @@ def get_control(env):
   return u
 
 with env.launch_passive_viewer() as viewer:
-  # Close the viewer automatically after 30 wall-seconds.
-  start = time.time()
-  while viewer.is_running():
-    print(f"[DEBUG] Iteration start")
-    step_start = time.time()
+  with media.VideoWriter(video_path, fps=video_fps, shape=video_resolution) as video:
+    # Close the viewer automatically after 30 wall-seconds.
+    start = time.time()
+    while viewer.is_running():
+      print(f"[DEBUG] Iteration start")
+      step_start = time.time()
 
-    # Step the simulation forward.
-    control_time = time.time()
-    u = get_control(env)
-    control_time = time.time() - control_time
-    print(f"[DEBUG] Total control time: {control_time}")
-    env_setp_time = time.time()
-    env.step(u)
-    env_setp_time = time.time() - env_setp_time
-    print(f"[DEBUG] Environment step time: {env_setp_time}")
+      # Step the simulation forward.
+      control_time = time.time()
+      u = get_control(env)
+      control_time = time.time() - control_time
+      print(f"[DEBUG] Total control time: {control_time}")
+      env_setp_time = time.time()
+      env.step(u)
+      env_setp_time = time.time() - env_setp_time
+      print(f"[DEBUG] Environment step time: {env_setp_time}")
 
-    # Pick up changes to the physics state, apply perturbations, update options from GUI.
-    viewer.sync()
+      # Pick up changes to the physics state, apply perturbations, update options from GUI.
+      viewer.sync()
+      
+      # Render video
+      if frame_count < env.time * video_fps:
+          renderer.update_scene(env.data, camera="top")
+          pixels = renderer.render()
+          video.add_image(pixels)
+          frame_count += 1
 
-    # Rudimentary time keeping, will drift relative to wall clock.
-    time_until_next_step = env.timestep - (time.time() - step_start)
-    if time_until_next_step > 0:
-      time.sleep(time_until_next_step)
+      # Rudimentary time keeping, will drift relative to wall clock.
+      time_until_next_step = env.timestep - (time.time() - step_start)
+      if time_until_next_step > 0:
+        time.sleep(time_until_next_step)
