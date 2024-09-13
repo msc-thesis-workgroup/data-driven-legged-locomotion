a3af9cce1ffb44129b07e21ce5a437f67ae496be
my-rice <dvdrsi2017@gmail.com>
2024-09-13 20:20:17 +0200

    [REF] cost function in h1_walk.py to include wall costs and adjust obstacle size scaling

--- data_driven_legged_locomotion/tasks/h1_walk/h1_walk.py
+++ data_driven_legged_locomotion/tasks/h1_walk/h1_walk.py
@@ -54,22 +54,48 @@ class Cost:
     def __init__(self, obstacles_positions, obstacles_sizes):
         self.obstacles_positions = obstacles_positions
         self.obstacles_sizes = obstacles_sizes
-        self.alpha = 400.0
+        self.alpha = 1000.0
+        self.beta = 10
 
     def get_cost_function(self):
         def cost(x, k):
             r = np.array([10.0, 10.0])
             z_torso = 1.06
-            costs = (x[:,0] - r[0])**2 + (x[:,1] - r[1])**2
+            costs = 20*(x[:,0] - r[0])**2 + (x[:,1] - r[1])**2 -10/(np.sqrt(( ((x[:,0]-r[0])/1000)**2 + ((x[:,1] - r[1])/1000)**2 +0.1)))
             costs = np.squeeze(costs)
-            costs += (x[:,2] - z_torso)**2
+            costs += self.alpha*(x[:,2] - z_torso)**2
 
             # obstacles are modeled as bivariate gaussian obstacles
             for i in range(len(self.obstacles_positions)):
                 obs = self.obstacles_positions[i]
-                size = self.obstacles_sizes[i]
+                size = self.beta*self.obstacles_sizes[i]
                 costs += self.alpha*np.exp(-((x[:,0] - obs[0])**2/(2*size[0]**2) + (x[:,1] - obs[1])**2/(2*size[1]**2)))
 
+
+            # Add wall costs. The walls are modeled as bivariate gaussian obstacles from the corners of the room: (-1,-1), (-1,11), (11,-1), (11,11).
+            x_s = np.array([-1, 11])
+            y_s = np.array([-1, 11])
+            wall_size = 1
+            wall_alpha = self.alpha
+            wall_cost = 0
+            for x_i in x_s:
+                wall_cost += wall_alpha*np.exp(-((x[:,0] - x_i)**2/(2*wall_size**2)))
+            for y_i in y_s:
+                wall_cost += wall_alpha*np.exp(-((x[:,1] - y_i)**2/(2*wall_size**2)))
+            costs += wall_cost
+            
             return costs
 
         return cost
+
+    # def get_cost_function(self):
+    #     def cost(x, k):
+    #         r = np.array([10.0, 0.0])
+    #         z_torso = 0.96
+    #         costs = 30*(x[:,0] - r[0])**4 + (x[:,1] - r[1])**4
+    #         costs = np.squeeze(costs)
+    #         costs += self.alpha*(x[:,2] - z_torso)**2
+            
+    #         return costs
+        
+    #     return cost
